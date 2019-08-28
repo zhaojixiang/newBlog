3c99c11ff59ed4962da30056bb8595357b8c0f51
zhaojixiang <1114981952@qq.com>
2019-08-28 16:44:22 +0800

    提交

--- src/components/HomePage/RightContent/index.js
+++ src/components/HomePage/RightContent/index.js
@@ -2,41 +2,47 @@ import React, { Component } from 'react'
 import style from './index.less'
 import new1 from '../../../assets/images/new1.jpg'
 import { Card } from 'antd';
+import { Consumer } from '../../../routes/HomePage/index'
 const { Meta } = Card;
-
 export default class RightContent extends Component {
   render () {
     return (
-      <div>
-        <Card
-          hoverable
-          style={{ width: '200px' }}
-          cover={<img alt="example" src={new1} />}
-        >
-          <Meta title="Europe Street beat" description="www.instagram.com" />
-        </Card>
-        <Card
-          hoverable
-          style={{ width: '200px' }}
-          cover={<img alt="example" src={new1} />}
-        >
-          <Meta title="Europe Street beat" description="www.instagram.com" />
-        </Card>
-        <Card
-          hoverable
-          style={{ width: '200px' }}
-          cover={<img alt="example" src={new1} />}
-        >
-          <Meta title="Europe Street beat" description="www.instagram.com" />
-        </Card>
-        <Card
-          hoverable
-          style={{ width: '200px' }}
-          cover={<img alt="example" src={new1} />}
-        >
-          <Meta title="Europe Street beat" description="www.instagram.com" />
-        </Card>
-      </div>
+      <Consumer>
+        {(theme) => {
+          <div>
+            {console.log(theme, 'theme')}
+            <Card
+              hoverable
+              style={{ width: '200px' }}
+              cover={<img alt="example" src={new1} />}
+            >
+              <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card>
+            <Card
+              hoverable
+              style={{ width: '200px' }}
+              cover={<img alt="example" src={new1} />}
+            >
+              <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card>
+            <Card
+              hoverable
+              style={{ width: '200px' }}
+              cover={<img alt="example" src={new1} />}
+            >
+              <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card>
+            <Card
+              hoverable
+              style={{ width: '200px' }}
+              cover={<img alt="example" src={new1} />}
+            >
+              <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card>
+          </div>
+        }}
+      </Consumer>
+
     )
   }
 }
--- src/routes/HomePage/index.js
+++ src/routes/HomePage/index.js
@@ -6,70 +6,75 @@ import ArticleOverviewL from '../../components/HomePage/ArticleOverviewL'
 import { Carousel, Icon, Pagination } from 'antd';
 import style from './index.less';
 
-function onShowSizeChange (current,pageSize) {
+function onShowSizeChange (current, pageSize) {
   console.log(current, pageSize);
 }
+
+export const { Provider, Consumer } = React.createContext()
 class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      theme: 'light'
     }
     this.carousel = null;
   }
   render () {
     return (
-      <div className={style.container}>
-        <div className={style.mainBox}>
-          {/* banner */}
-          <div className={style.banner}>
-            <Icon onClick={this.goLeft} type="left" className={[style.left, style.icon]} />
-            <Carousel autoplay ref={e => { this.carousel = e }}>
-              <div><h3>1</h3></div>
-              <div><h3>2</h3></div>
-              <div><h3>3</h3></div>
-              <div><h3>4</h3></div>
-            </Carousel>
-            <Icon onClick={this.goRight} type="right" className={[style.right, style.icon]} />
-          </div>
-
-          <div className={style.contentBox}>
-            <div className={style.leftContent}>
-              {/* 最近三个动态 */}
-              <RecentNews />
-              {/* 动态列表 */}
-              <div className={style.articalList} >
-                <div className={style.articalItem} >
-                  <ArticleOverviewR />
-                </div>
-                <div className={style.articalItem} >
-                  <ArticleOverviewL />
-                </div>
-                <div className={style.articalItem} >
-                  <ArticleOverviewR />
-                </div>
-                <div className={style.articalItem} >
-                  <ArticleOverviewL />
-                </div>
-              </div>
-              {/* 分页组件 */}
-              <div className={style.paging}>
-
-                <Pagination
-                  showSizeChanger
-                  onShowSizeChange={onShowSizeChange}
-                  defaultCurrent={3}
-                  total={50}
-                />
-              </div>
+      <Provider value={this.state}>
+        <div className={style.container}>
+          <div className={style.mainBox}>
+            {/* banner */}
+            <div className={style.banner}>
+              <Icon onClick={this.goLeft} type="left" className={[style.left, style.icon]} />
+              <Carousel autoplay ref={e => { this.carousel = e }}>
+                <div><h3>1</h3></div>
+                <div><h3>2</h3></div>
+                <div><h3>3</h3></div>
+                <div><h3>4</h3></div>
+              </Carousel>
+              <Icon onClick={this.goRight} type="right" className={[style.right, style.icon]} />
             </div>
-            {/* 右侧内容 */}
-            <div className={style.rightContent}>
-              <RightContent />
+
+            <div className={style.contentBox}>
+              <div className={style.leftContent}>
+                {/* 最近三个动态 */}
+                <RecentNews />
+                {/* 动态列表 */}
+                <div className={style.articalList} >
+                  <div className={style.articalItem} >
+                    <ArticleOverviewR />
+                  </div>
+                  <div className={style.articalItem} >
+                    <ArticleOverviewL />
+                  </div>
+                  <div className={style.articalItem} >
+                    <ArticleOverviewR />
+                  </div>
+                  <div className={style.articalItem} >
+                    <ArticleOverviewL />
+                  </div>
+                </div>
+                {/* 分页组件 */}
+                <div className={style.paging}>
+
+                  <Pagination
+                    showSizeChanger
+                    onShowSizeChange={onShowSizeChange}
+                    defaultCurrent={3}
+                    total={50}
+                  />
+                </div>
+              </div>
+              {/* 右侧内容 */}
+              <div className={style.rightContent}>
+
+                <RightContent />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Provider>
     )
   }
   goLeft = () => {
